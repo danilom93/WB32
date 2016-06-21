e9b62a61d9b6689eb7b9085340ccfbb6b95d0201
ANTiPoDE-Dev <git@antipode-dev.org>
2016-06-22 00:41:26 +0200

    commit per sync

--- mplabx/WB32.X/main.cpp
+++ mplabx/WB32.X/main.cpp
@@ -1,19 +1,55 @@
+//#include "ACore.h"
+//#include "ALcd.h"
+//#include "APCF8563.h"
+//
+//#include <sys/attribs.h>
+//
+//
+//using namespace AFramework;
+//
+//int main(int argc, char** argv) {
+//    
+//    System::init(16392, &PortB, bit15);
+//    
+//    APCF8563 clock(&I2C2);
+//    
+//    ALcd lcd(&PortC, bit0, &PortC, bit1, &PortC, bit2, &PortC, bit3, &PortC, bit4, &PortC, bit5, &PortC, bit6);
+//    
+//    System::delay(1000);
+//    
+//    lcd.lightOn();
+//
+//    lcd << "test";
+//    
+//    System::delay(1000);
+//    if(clock.isGood()){
+//        
+//        lcd << "clock ok";
+//    }else{
+//        lcd << "clock fail";   
+//    }
+//    clock.setClockOut(APCF8563::Clock1Hz);
+//    ADateTime tm;
+//    while(1){
+//        lcd.clear();
+//        tm = clock.currentTime();
+//        lcd << tm.timeToString();
+//        System::delay(1000);
+//    }
+//}
+
+
 #include "ACore.h"
 #include "ALcd.h"
 #include "AESP8266.h"
-
+#include "A24LC512.h"
+#include "APCF8563.h"
+#include "AXbee.h"
+#include "MTemp.h"
+#include "MTempDefs.h"
 #include <sys/attribs.h>
 
-
 using namespace AFramework;
-
-extern "C"{
-    
-    void __ISR(_UART_1_VECTOR, IPL1AUTO) UART_HANDLER(){
-        
-        UART1.rxHandler();
-    }
-}
 
 int main(int argc, char** argv) {
     
@@ -23,69 +59,24 @@ int main(int argc, char** argv) {
     
     ALcd lcd(&PortC, bit0, &PortC, bit1, &PortC, bit2, &PortC, bit3, &PortC, bit4, &PortC, bit5, &PortC, bit6);
     
-    AESP8266 wlan(&UART1);
-    
-    UART1.getReceiverEventController()->enableInterrupt(Ip1, Isp0);
-    
-    System::outputMap(RPA0R, U1TXR);
-    System::inputMap(RPA4R, U1RXR);
+        
+    APCF8563    clock(&I2C2);
     
     System::delay(1000);
     
     lcd.lightOn();
-
+        
+    
+    ADateTime tmp;
+    lcd << "PROVA\n";
+    clock.setClockOut(APCF8563::Clock1KHz);
+    System::delay(1000);
     while(1){
+        
         lcd.clear();
-        lcd << "MEM: " <<AString(static_cast<sint32>(System::memstat()));
-        System::delay(2000);
-        if(wlan.isOk()){
-            lcd.clear();
-            lcd << "Modulo ok";
-            System::delay(1000);
-            lcd.clear();
-            lcd << "Configuro\nstazione...";
-            System::delay(1000);
-
-            if(wlan.setMode(AESP8266::StationMode)){
-                lcd.clear();
-                lcd << "Stazione OK!";
-                System::delay(1000);
-                
-                if(wlan.joinAP("AndroidAP", "danilom93")){
-                    lcd.clear();
-                    lcd << "Connesso!\n";
-                    System::delay(1000);
-                    lcd << "Lascio AP...";
-                    
-                    if(wlan.leaveAP()){
-                        lcd.clear();
-                        lcd << "AP lasciato.";
-                        System::delay(1000);      
-                        
-                    }else{
-                        
-                        lcd.clear();
-                        lcd << "Errore AP.";
-                        System::delay(1000);                        
-                    }
-                }else{
-                    
-                    lcd.clear();
-                    lcd << "Non Connesso!";
-                    System::delay(1000);
-                }
-            }else{
-                
-                lcd.clear();
-                lcd << "Stazione\nFallita!";
-            }
-        }else{
-            
-            lcd.clear();
-            lcd << "ESP FAIL...";
-        }
-        System::delay(2000);
+        tmp = clock.currentTime();
+        lcd << "SONO LE ORE\n";
+        lcd << tmp.timeToString();
+        System::delay(1000);
     }
 }
-
-
